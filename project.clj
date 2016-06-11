72f726f4ba94a4ff041f0382d38780e269552ff5
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-11 19:45:37 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/system.sente "0.1.0"
+(defproject sv/system.sente "0.1.1-SNAPSHOT"
   :description "Components and helper functions to use sente in a
                 sv/system-based application."
   :url "https://github.com/SimpleValue/sv.system.sente"
